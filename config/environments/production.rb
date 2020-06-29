e05231f6592725d7d6c4e6f6517b7b333f007f8a
Lucille James <lucille.james00@gmail.com>
2020-06-29 11:56:35 +0200

    changed api key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,19 +86,15 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors= true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                      587,
-    domain:                 'gmail.com',
-    user_name:           ENV["SENDGRID_USERNAME"],
-    password:              ENV["SENDGRID_PASSWORD"],
-    api_key:              ENV["SENDGRID_API_KEY"],
-    authentication:      'plain',
-    enable_starttls_auto: true  }
-
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'SENDGRID_USERNAME',
+  :api_key => 'SENDGRID_API_KEY',
+  :domain => 'suite-and-caux.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
