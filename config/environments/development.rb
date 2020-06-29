4d2ff14fc0fc867fd0b98ff5a723b014b304ecad
Lucille James <lucille.james00@gmail.com>
2020-06-29 12:54:31 +0200

    changed .env var

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,5 @@ Rails.application.configure do
     password:              ENV["GMAIL_PASSWORD"],
     authentication:      'plain',
     enable_starttls_auto: true  }
+    
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,18 +86,31 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.raise_delivery_errors= true
+#   config.action_mailer.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => 'SENDGRID_USERNAME',
+#   :api_key => 'SENDGRID_API_KEY',
+#   :domain => 'suite-and-caux.herokuapp.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 465,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'SENDGRID_USERNAME',
-  :api_key => 'SENDGRID_API_KEY',
-  :domain => 'suite-and-caux.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 465,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                      587,
+    domain:                 'gmail.com',
+    user_name:           ENV["GMAIL_USERNAME"],
+    password:              ENV["GMAIL_PASSWORD"],
+    authentication:      'plain',
+    enable_starttls_auto: true  }
+    
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
