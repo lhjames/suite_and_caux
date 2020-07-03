7271edca79a229a5ce5fb1af901740776b047205
Lucille James <lucille.james00@gmail.com>
2020-07-03 11:39:10 +0200

    debugged jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
+//= require turbolinks
 //= require_tree .
+//= require bootstrap
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
