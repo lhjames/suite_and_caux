531ea9154adc0e942895a6ffd0c3750b5b4eb3dd
Lucille James <lucille.james00@gmail.com>
2020-07-04 11:32:35 +0200

    installed font-awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ require('jquery')
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
-
+//= require font-awesome
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
