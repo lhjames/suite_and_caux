a4eaefb0baade095f8c1ae55553b971752ce88da
Lucille James <lucille.james00@gmail.com>
2020-09-08 11:43:14 +0200

    added English show version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,4 @@ around_action :switch_locale
   	I18n.with_locale(locale, &action)
    end
 
-	# def set_locale
-	# 	locale = params[:locale].to_s.strip.to_sym
-	# 	I18n.locale = I18n.available_locales.include?(locale) ?
-	# 		locale : 
-	# 		I18n.default_locale
-	# end
-
 end
